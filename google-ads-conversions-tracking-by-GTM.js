93846f604538143b8197cd18528412ac67cb7b98
Ahmad Ismail <74207341+ahmadoqsrawi@users.noreply.github.com>
2023-06-20 12:11:42 +0200

    Update google-ads-conversions-tracking-by-GTM.js
    1- Use a switch statement instead of multiple if-else conditions: Since the triggerType determines the type of trigger, we can use a switch statement to handle each case more efficiently.
    
    2- Use object literals for trigger configurations: Instead of defining separate objects for each trigger type, we can use an object literal to store the common properties and override the specific properties for each trigger type.

--- google-ads-conversions-tracking-by-GTM.js
+++ google-ads-conversions-tracking-by-GTM.js
@@ -1,62 +1,49 @@
-// Version: 1.0.1
-
+// Version: 1.0.2
 // MIT License
 
 function createGoogleAdsConversionTag(triggerType, triggerFilters) {
-    var tagManager = TagManager.Accounts.get('123456'); // Your GTM Account ID
-    var container = tagManager.containers.get('GTM-123456'); // Your GTM Container ID
+  var tagManager = TagManager.Accounts.get('123456'); // Your GTM Account ID
+  var container = tagManager.containers.get('GTM-123456'); // Your GTM Container ID
   
-    var trigger = null;
-    if (triggerType === 'click') {
-      // Create a Click trigger
-      trigger = container.createTrigger('click', {
-        'name': 'All Clicks',
-        'auto_event_filter': 'link click, button click',
-        'some_custom_event_filters': triggerFilters
-      });
-    } else if (triggerType === 'domready') {
-      // Create a DOM Ready trigger
-      trigger = container.createTrigger('domready', {
-        'name': 'DOM Ready',
-        'some_custom_event_filters': triggerFilters
-      });
-    } else if (triggerType === 'windowload') {
-      // Create a Window Loaded trigger
-      trigger = container.createTrigger('windowload', {
-        'name': 'Window Loaded',
-        'some_custom_event_filters': triggerFilters
-      });
-    } else if (triggerType === 'justlinks') {
-      // Create a Just Links trigger
-      trigger = container.createTrigger('click', {
-        'name': 'Just Links',
-        'auto_event_filter': 'link click',
-        'some_custom_event_filters': triggerFilters
-      });
-    } else if (triggerType === 'formsubmit') {
-      // Create a Form Submission trigger
-      trigger = container.createTrigger('submit', {
-        'name': 'Form Submission',
-        'some_custom_event_filters': triggerFilters
-      });
-    } else if (triggerType === 'custom') {
-      // Create a Custom Event trigger
-      trigger = container.createTrigger('custom', {
-        'name': 'Custom Event',
-        'some_custom_event_filters': triggerFilters
-      });
-    }
-    
-    var conversionTag = container.createTag('adsconversion', {
-      'name': 'Google Ads Conversion Tracking',
-      'conversion_id': '***********', // Your Conversion ID
-      'conversion_label': '***********r', // Your Conversion Label
-      'send_to': '***********' // Your Google Ads Conversion ID and Label
-    });
+  var triggerConfig = {
+    'name': '',
+    'some_custom_event_filters': triggerFilters
+  };
   
-    container.createTriggerLink(conversionTag, trigger);
-  
-    container.save();
-    Logger.log('The tag has been created.');
+  switch (triggerType) {
+    case 'click':
+      triggerConfig.name = 'All Clicks';
+      triggerConfig.auto_event_filter = 'link click, button click';
+      break;
+    case 'domready':
+      triggerConfig.name = 'DOM Ready';
+      break;
+    case 'windowload':
+      triggerConfig.name = 'Window Loaded';
+      break;
+    case 'justlinks':
+      triggerConfig.name = 'Just Links';
+      triggerConfig.auto_event_filter = 'link click';
+      break;
+    case 'formsubmit':
+      triggerConfig.name = 'Form Submission';
+      break;
+    case 'custom':
+      triggerConfig.name = 'Custom Event';
+      break;
   }
   
+  var trigger = container.createTrigger(triggerType, triggerConfig);
+  
+  var conversionTag = container.createTag('adsconversion', {
+    'name': 'Google Ads Conversion Tracking',
+    'conversion_id': '***********', // Your Conversion ID
+    'conversion_label': '***********r', // Your Conversion Label
+    'send_to': '***********' // Your Google Ads Conversion ID and Label
+  });
+  
+  container.createTriggerLink(conversionTag, trigger);
+  
+  container.save();
+  Logger.log('The tag has been created.');
+}
